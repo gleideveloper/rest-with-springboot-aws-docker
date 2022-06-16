377519496a700695fca74531651a8ad4e80c689e
Gleides Vinente <gleidevelop@gmail.com>
2022-06-16 16:14:57 -0400

    Projeto Inicial

---
+++ src/test/java/com/example/restwithspringbootawsdocker/RestWithSpringbootAwsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restwithspringbootawsdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringbootAwsDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
