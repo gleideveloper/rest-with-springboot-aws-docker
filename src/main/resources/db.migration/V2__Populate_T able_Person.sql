30b4d93726263936dc26e6456d44c6a48034fb0c
Gleides Vinente <gleidevelop@gmail.com>
2022-06-21 19:24:16 -0400

    Versionamento database com flywaydb

---
+++ src/main/resources/db.migration/V2__Populate_T able_Person.sql
@@ -0,0 +1,6 @@
+insert into tbl_person (id, first_name, last_name, address, gender)
+values  (1, 'Ana Rosa', 'Da Silva', 'Rua Goiás, 28 Manaus - AM - Brasil', 'Female'),
+        (2, 'Gleides Vinente', 'Da Silva', 'Rua Goiás, 28 Manaus - AM - Brasil', 'Male'),
+        (3, 'Joãozinho Vinente', 'Monteiro', 'Rua Goiás, 401 Manaus - AM - Brasil', 'Male'),
+        (4, 'Cicrano', 'Bezerra', 'Belém/Pa - Brasil', 'Male'),
+        (5, 'Beltrano', 'Sikeira', 'Recife/Pe - Brasil', 'Male');
